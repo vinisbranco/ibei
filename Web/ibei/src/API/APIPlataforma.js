df74724bbc65d1640732c8addcadd920dc3f53c0
Bruno Lippert <brunohlippert@gmail.com>
2019-08-09 13:49:48 -0300

    fim

--- Web/ibei/src/API/APIPlataforma.js
+++ Web/ibei/src/API/APIPlataforma.js
@@ -4,38 +4,43 @@ import web3 from '../Web3';
 export const getProduto = async (id) => {
     var response = await Plataforma.methods.getProduto(id).call().then(
         (result) => {
-            console.log(result);
+            return result;
         }
     );
     return response;
 }
 
-export const getProdutosLoja = async (dono) => {
-    var response = await Plataforma.methods.getProdutosLoja(dono).call().then(
-        (result) => {
-            console.log(result);
-        }
-    );
-    return response;
-}
-
-export const cadastraProduto = async (id, nome, descricao, preco) => {
+export const getProdutosLoja = async () => {
     var chaveUsuario;
-    web3.eth.getCoinbase(async (e, account) => {
+    await web3.eth.getCoinbase(async (e, account) => {
         if (account !== null) {
             chaveUsuario = account;
         }
     })
-    await contratoSistema.methods.cadastraProduto(id, nome, descricao, preco)
+    var response = await Plataforma.methods.getProdutosLoja(chaveUsuario).call();
+    return response;
+}
+
+export const cadastraProduto = async (nome, descricao, preco) => {
+    var chaveUsuario;
+    await web3.eth.getCoinbase(async (e, account) => {
+        if (account !== null) {
+            chaveUsuario = account;
+        }
+    })
+    var id = await web3.eth.accounts.create();
+    id = id.address;
+
+    await Plataforma.methods.cadastraProduto(id, nome, descricao, preco)
         .send({ from: chaveUsuario })
         .then(
             (result) => {
-                return (result);
+                return true;
             }
         )
         .catch(
             (erro) => {
-                return (erro);
+                return false;
             }
         );
 }
--- Web/ibei/src/Body.js
+++ Web/ibei/src/Body.js
@@ -11,22 +11,26 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import { getProdutosLoja, cadastraProduto, getProduto } from './API/APIPlataforma';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   Title: {
-    color: 'white',
+    color: 'gray',
     fontSize: 40,
     padding: '30px 0px 30px 0px'
   },
   caixaRegistra: {
     padding: '30px',
     borderRadius: 6,
-    backgroundColor: '#DCDCDC',
+    marginLeft: '10px',
+    backgroundColor: '#FFFAFA',
   },
   caixaTabela: {
     padding: '30px',
     borderRadius: 6,
-    backgroundColor: '#DCDCDC',
+    marginRight: '10px',
+    backgroundColor: '#FFFAFA',
   },
   subtitulo: {
     color: 'gray',
@@ -41,17 +45,44 @@ const styles = theme => ({
 });
 
 function createData(produto, preco, descricao, status) {
-  return { produto, preco, descricao, status};
+  return { produto, preco, descricao, status };
 }
 
-const rows = [
-  createData('Ford Focus 2016', '42623.99', 'Ar, Airbag, Direção Hidraulica', false),
-];
+
+
 
 class Body extends React.Component {
   state = {
-    value: 0,
+    produto: '',
+    descricao: '',
+    preco: '',
+    rows: [],
+    cadastrando: false
   };
+
+  componentDidMount() {
+    this.buscaDados();
+  }
+
+  cadastraProduto = async () => {
+    this.setState({ cadastrando: true })
+    var result = await cadastraProduto(this.state.produto, this.state.descricao, parseInt(this.state.preco.replace(".", "")) * 100);
+    await this.buscaDados();
+    this.setState({ cadastrando: false, produto: '', descricao: '', preco: '' })
+  }
+
+  buscaDados = async () => {
+    var result = await getProdutosLoja();
+    var rowAux = [];
+    for (var i = 0; i < result.length; i++) {
+      var resp = await getProduto(result[i]);
+      rowAux.push(createData(resp.nome, resp.preco / 100, resp.descricao, resp.estaVendido));
+    }
+
+    this.setState({
+      rows: rowAux
+    })
+  }
 
   handleChange(event) {
     const target = event.target.id;
@@ -63,98 +94,115 @@ class Body extends React.Component {
     const { classes } = this.props;
     const { value } = this.state;
     return (
-      <div className={classes.root}>
-        <Grid container>
-          <Grid item xs={12} md={12} lg={12} xl={12}>
-            <Typography align="center" className={classes.Title} >
-              ShopChain
+      (this.state.cadastrando) ?
+        <div className={classes.root}>
+          <Grid container>
+            <Grid item xs={12} md={12} lg={12} xl={12}>
+              <Typography align="center" className={classes.Title} >
+                ShopChain
               </Typography>
+            </Grid>
+            <CircularProgress style={{marginLeft: '48%'}}></CircularProgress>
+            <Grid item xs={12} md={12} lg={12} xl={12}>
+              <Typography align="center" className={classes.Title} >
+                Sua requisição está sendo processada!
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid className={classes.ladoEsquerdo} item xs={4} md={4} lg={4} xl={4}>
-            <Paper className={classes.caixaRegistra} elevation={1}>
-              <Typography align="center" className={classes.subtitulo}>
-                Vender produto
+        </div>
+        :
+        <div className={classes.root}>
+          <Grid container>
+            <Grid item xs={12} md={12} lg={12} xl={12}>
+              <Typography align="center" className={classes.Title} >
+                ShopChain
+              </Typography>
+            </Grid>
+            <Grid className={classes.ladoEsquerdo} item xs={12} md={4} lg={4} xl={4}>
+              <Paper className={classes.caixaRegistra} elevation={1}>
+                <Typography align="center" className={classes.subtitulo}>
+                  Vender produto
               </Typography>
 
-              <Grid item xs={12} md={12} lg={12} xl={12}>
-                <TextField
-                  id="produto"
-                  label="Produto"
-                  placeholder="Carro, Caneca, Camiseta..."
-                  fullWidth
-                  value={this.state.produto}
-                  onChange={this.handleChange.bind(this)}
-                  margin="normal"
-                  variant="outlined"
-                />
-              </Grid>
-              <Grid item xs={12} md={12} lg={12} xl={12}>
-                <TextField
-                  id="descricao"
-                  label="Descrição"
-                  placeholder="Cor, Ano, Modelo..."
-                  fullWidth
-                  value={this.state.descricao}
-                  onChange={this.handleChange.bind(this)}
-                  margin="normal"
-                  variant="outlined"
-                />
-              </Grid>
-              <Grid item xs={12} md={12} lg={12} xl={12}>
-                <TextField
-                  id="preco"
-                  label="Preço"
-                  placeholder="69.99"
-                  fullWidth
-                  value={this.state.preco}
-                  onChange={this.handleChange.bind(this)}
-                  margin="normal"
-                  variant="outlined"
-                />
-              </Grid>
+                <Grid item xs={12} md={12} lg={12} xl={12}>
+                  <TextField
+                    id="produto"
+                    label="Produto"
+                    placeholder="Carro, Caneca, Camiseta..."
+                    fullWidth
+                    value={this.state.produto}
+                    onChange={this.handleChange.bind(this)}
+                    margin="normal"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} md={12} lg={12} xl={12}>
+                  <TextField
+                    id="descricao"
+                    label="Descrição"
+                    placeholder="Cor, Ano, Modelo..."
+                    fullWidth
+                    value={this.state.descricao}
+                    onChange={this.handleChange.bind(this)}
+                    margin="normal"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} md={12} lg={12} xl={12}>
+                  <TextField
+                    id="preco"
+                    label="Preço"
+                    placeholder="69.99"
+                    fullWidth
+                    value={this.state.preco}
+                    onChange={this.handleChange.bind(this)}
+                    margin="normal"
+                    variant="outlined"
+                  />
+                </Grid>
 
-              <Grid item xs={12} md={12} lg={12} xl={12}>
-                <div align="center">
-                  <Button variant="contained" color="primary" className={classes.button} onClick={() => console.log("oi")}>
-                    Anunciar
+                <Grid item xs={12} md={12} lg={12} xl={12}>
+                  <div align="center">
+                    <Button variant="contained" color="primary" className={classes.button} onClick={() => this.cadastraProduto()}>
+                      Anunciar
                   </Button>
-                </div>
-              </Grid>
-            </Paper>
-          </Grid>
-          <Grid item xs={1} md={1} lg={1} xl={1}>
-          </Grid>
-          <Grid className={classes.ladoDireito} item xs={7} md={7} lg={7} xl={7}>
-            <Paper className={classes.caixaTabela} elevation={1}>
-              <Typography align="center" className={classes.subtitulo}>
-                Meus produtos
+                  </div>
+                </Grid>
+              </Paper>
+            </Grid>
+            <Grid item xs={1} md={1} lg={1} xl={1}>
+            </Grid>
+            <Grid className={classes.ladoDireito} item xs={12} md={7} lg={7} xl={7}>
+              <Paper className={classes.caixaTabela} elevation={1}>
+                <Typography align="center" className={classes.subtitulo}>
+                  Meus produtos
               </Typography>
-              <Table className={classes.table}>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Produto</TableCell>
-                    <TableCell align="right">Preço</TableCell>
-                    <TableCell align="right">Descrição</TableCell>
-                    <TableCell align="right">Status</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map(row => (
-                    <TableRow key={row.name}>
-                      <TableCell component="th" scope="row">
-                        {row.produto}
-                      </TableCell>
-                      <TableCell align="right">R$ {row.preco}</TableCell>
-                      <TableCell align="right">{row.descricao}</TableCell>
-                      <TableCell align="right">{row.status ? "Vendido" : "Disponível"}</TableCell>
+                <Table className={classes.table}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Produto</TableCell>
+                      <TableCell align="right">Preço</TableCell>
+                      <TableCell align="right">Descrição</TableCell>
+                      <TableCell align="right">Status</TableCell>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Paper>
+                  </TableHead>
+                  <TableBody>
+                    {this.state.rows.map(row => (
+                      <TableRow key={row.name}>
+                        <TableCell component="th" scope="row">
+                          {row.produto}
+                        </TableCell>
+                        <TableCell align="right">R$ {row.preco}</TableCell>
+                        <TableCell align="right">{row.descricao}</TableCell>
+                        <TableCell align="right">{row.status ? "Vendido" : "Disponível"}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Paper>
+            </Grid>
           </Grid>
-        </Grid>
-      </div>
+        </div>
     );
   }
 }
